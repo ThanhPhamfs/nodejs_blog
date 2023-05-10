d4c0e8bdcac08b2b9457cebfb450819b0df7ed6f
Pham Van Thanh <phamthanh.it09@gmail.com>
2023-05-10 21:14:49 +0700

    fix error: require() of ES Module

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,27 +1,27 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const db = require("../../config/db");
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../../config/db');
 
-const Course = db.sequelize.define("courses", {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  image: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
+const Course = db.sequelize.define('courses', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
 });
 
 (async () => {
-  try {
-    await db.sequelize.sync();
-  } catch (error) {
-    console.log("Sync Course model with db fail: " + String(error))
-  }
+    try {
+        await db.sequelize.sync();
+    } catch (error) {
+        console.log('Sync Course model with db fail: ' + String(error));
+    }
 })();
 
-module.exports = Course
+module.exports = Course;
